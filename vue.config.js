216aca48064631e5fb89d15923ddaffbbb9abb87
LiChaoJun <lichaojun@deltaphone.com.cn>
2022-04-19 10:30:38 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 /*
  * @Author: LiChaoJun
  * @Date: 2022-04-18 18:00:49
- * @LastEditTime: 2022-04-18 18:01:59
+ * @LastEditTime: 2022-04-19 10:24:12
  * @LastEditors: LiChaoJun
  * @Description: vue.config.js
  */
 module.exports = {
     outputDir: 'docs',
+    publicPath: process.env.NODE_ENV === 'production' ? '/template' : '/',
 };
